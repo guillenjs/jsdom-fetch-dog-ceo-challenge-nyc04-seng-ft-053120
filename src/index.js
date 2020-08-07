73a1b395f3d89e1ce9ca9a92d0e2130b4956b4e2
guillenjs <jorgeguilln@gmail.com>
2020-08-07 10:27:06 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
 console.log('%c HI', 'color: firebrick')
+
+
+
+fetch('https://dog.ceo/api/breeds/image/random/4') // fetch url
+.then(response => response.json())
+  .then(data => renderDogs(data));
+
+function renderDogs(images) {
+    const container = document.querySelector('div')
+    images.message.forEach ( url => {
+        let imageTag = document.createElement('img')
+        imageTag.src = url
+        container.append(imageTag)
+    })
+} 
+
+
+
+fetch('https://dog.ceo/api/breeds/list/all')
+    .then(response => response.json())
+    .then(data => renderBreeds(Object.keys(data.message)) )
+
+function renderBreeds(breeds) {
+    const listContainer = document.querySelector('ul')
+    breeds.forEach ( breed => {
+        let newLi = document.createElement('li')
+        newLi.innerHTML = breed
+        listContainer.append(newLi)
+    })
+}
+
+
